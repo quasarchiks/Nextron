354320f86a1b17e50e35ab2ccd331f9e12de7872
quasar <quasarchiks@gmail.com>
2025-03-24 08:56:09 +0600

    Main Button With texture

--- assets/shaders/vertex.glsl
+++
@@ -1,6 +0,0 @@
-#version 330 core
-layout (location = 0) in vec2 aPos;
-uniform vec2 offset;
-void main() {
-    gl_Position = vec4(aPos + offset, 0.0, 1.0);
-}
--- engine/input/Input.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "engine/core/Window.h"
-
-class Input {
-public:
-    static void process(Window& window);
-};
